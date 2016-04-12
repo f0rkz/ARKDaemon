e7859bf79d0366cdec37d0b8b902a2cad5932124
f0rkz <f0rkz@f0rkznet.net>
2016-04-12 17:21:45 -0400

    Make it shiny.

--- ARKDaemon.py
+++ ARKDaemon.py
@@ -199,8 +199,8 @@ elif args.status:
         # Get the CPU times and memory load
         system_status = ArkServer(config=server_config)
         stats = system_status.sys_status()
-        print "CPU Usage: {}".format(stats.cpu_percent())
-        print "Memory Usage: {}".format(stats.memory_percent())
+        print "CPU Usage: {}%".format(int(stats.cpu_percent()))
+        print "Memory Usage: {}%".format(int(stats.memory_percent()))
         print "Thread Count: {}".format(stats.num_threads())
     else:
         print("Status: " + Fore.RED + "Offline" + Style.RESET_ALL)
