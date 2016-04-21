14188033ad756e2e09b72663998db5befc6ee957
f0rkz <f0rkz@f0rkznet.net>
2016-04-20 23:01:54 -0400

    it would help if I fixed it in the right place.

--- ARKDaemon/ArkBackup.py
+++ ARKDaemon/ArkBackup.py
@@ -21,8 +21,8 @@ class ArkBackup(object):
     def do_backup(self):
         # Do a saveworld operation
         result = {}
-        this = ServerQuery(ip='127.0.0.1', port=int(self.config['ARK']['query_port']))
-        query = this.status(config=self.config)
+        this = ServerQuery(ip='127.0.0.1', port=int(self.config['ARK']['query_port']), config=self.config)
+        query = this.status()
         if query['status'] is True:
             rcon = ServerRcon(ip='127.0.0.1',
                               port=int(self.config['ARK']['rcon_port']),
